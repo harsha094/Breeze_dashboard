60d8a98a51e91aa785e052c1449c3d2554a7e13a
Harsh <harsh.raghuwanshi@z1media.com>
2023-04-17 06:53:41 +0530

    laravel operations

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+    //get data 
+    // $user= DB::select('select * from users');
+    //$user= DB::insert('insert into users(name, email, password) values(?,?,?)',['Shourya','shourya1@gmail.com','12345678']);
+    // $user= DB::table('users')->where('name', 'Harsha')->value('email');
+    // $user = User::all();
+            // ->select('name', 'email as user_email')
+            // ->get();
+    // dd($user);
+    
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
